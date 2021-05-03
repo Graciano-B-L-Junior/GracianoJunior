2584b112e2988e4906e71c0be62cfa597be82a29
Graciano Junior <gracianobarbosalimajunior@gmail.com>
2021-05-03 15:50:46 -0300

    adicionado mais telas de projetos

--- functions.js
+++ functions.js
@@ -1,3 +1,23 @@
 $('.mobile > a').click(()=>{
     $('.mobile ul').slideToggle()
 })
+
+$('div[PJ_Pokedex]').click(()=>{
+    window.location.href =   'https://graciano-b-l-junior.github.io/pokedex/'
+})
+$('div[PJ_flexbox]').click(()=>{
+    window.location.href =   'https://graciano-b-l-junior.github.io/Projeto_site_02/'
+})
+$('div[PJ_adocao]').click(()=>{
+    window.location.href =   'https://github.com/Graciano-B-L-Junior/adocao_de_bolso_react'
+})
+$('div[PJ_SiteResponsivo]').click(()=>{
+    window.location.href =   'https://graciano-b-l-junior.github.io/projeto_site_06/'
+})
+$('div[PJ_appAlarme]').click(()=>{
+    window.location.href =   'https://github.com/Graciano-B-L-Junior/app_Alarme'
+})
+$('div[PJ_spotfy_mini_clone]').click(()=>{
+    window.location.href =   'https://github.com/Graciano-B-L-Junior/react_spotfy_mini_clone'
+})
+
